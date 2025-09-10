4507f0c972d02fda543aade6d46e8da8b7a75709
3KR4 <3KR4@GMAIL.COM>
2025-09-10 02:37:40 +0200

    aa

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -6,36 +6,45 @@ import Image from "next/image";
 
 export default function () {
   return (
-    <ul className="tools ">
-      <li data-aos="zoom-in" data-aos-delay="100">
-        <Image className="black" src={`/tools/1.png`} alt={`Ready Mode`} fill />
-        <h4>Ready Mode</h4>
-      </li>
-      <li data-aos="zoom-in" data-aos-delay="250">
-        <Image className="black" src={`/tools/2.png`} alt={`Dialpad`} fill />
-        <h4>Dialpad</h4>
-      </li>
-      <li data-aos="zoom-in" data-aos-delay="450">
-        <Image className="black" src={`/tools/3.png`} alt={`Resimpli`} fill />
-        <h4>Resimpli</h4>
-      </li>
-      <li data-aos="zoom-in" data-aos-delay="650">
-        <Image
-          className="black"
-          src={`/tools/4.png`}
-          alt={`Prop Stream`}
-          fill
-        />
-        <h4>Prop Stream</h4>
-      </li>
-      <li data-aos="zoom-in" data-aos-delay="850">
-        <Image src={`/tools/5.png`} alt={`Go High Level`} fill />
-        <h4>Go High Level</h4>
-      </li>
-      <li data-aos="zoom-in" data-aos-delay="1050">
-        <Image className="black" src={`/tools/6.png`} alt={`Podio`} fill />
-        <h4>Podio</h4>
-      </li>
-    </ul>
+    <div className="tools ">
+      <h1 data-aos="fade-up">our tools</h1>
+
+      <ul className="container">
+        <li data-aos="zoom-in" data-aos-delay="100">
+          <Image
+            className="black"
+            src={`/tools/1.png`}
+            alt={`Ready Mode`}
+            fill
+          />
+          <h4>Ready Mode</h4>
+        </li>
+        <li data-aos="zoom-in" data-aos-delay="250">
+          <Image className="black" src={`/tools/2.png`} alt={`Dialpad`} fill />
+          <h4>Dialpad</h4>
+        </li>
+        <li data-aos="zoom-in" data-aos-delay="450">
+          <Image className="black" src={`/tools/3.png`} alt={`Resimpli`} fill />
+          <h4>Resimpli</h4>
+        </li>
+        <li data-aos="zoom-in" data-aos-delay="650">
+          <Image
+            className="black"
+            src={`/tools/4.png`}
+            alt={`Prop Stream`}
+            fill
+          />
+          <h4>Prop Stream</h4>
+        </li>
+        <li data-aos="zoom-in" data-aos-delay="850">
+          <Image src={`/tools/5.png`} alt={`Go High Level`} fill />
+          <h4>Go High Level</h4>
+        </li>
+        <li data-aos="zoom-in" data-aos-delay="1050">
+          <Image className="black" src={`/tools/6.png`} alt={`Podio`} fill />
+          <h4>Podio</h4>
+        </li>
+      </ul>
+    </div>
   );
 }
